cd124dac9a988573b6276e16e4f35f61dc8cf0ca
Daniel Newton <kdaniel.newton@gmail.com>
2025-03-01 17:06:36 -0600

    sort out zsh

--- packages/common/fzf.sh
+++ packages/common/fzf.sh
@@ -12,13 +12,17 @@ get_latest_version() {
 	curl -s https://api.github.com/repos/junegunn/fzf/releases/latest | grep -o '"tag_name": *"[^"]*"' | cut -d'"' -f4 | sed 's/^v//'
 }
 
-current=$(get_current_version)
+if command -v fzf &> /dev/null; then
+	current=$(get_current_version)
+else
+	current=""
+fi
 latest=$(get_latest_version)
 
 if [ -z "$current" ] || [ "$current" != "$latest" ]; then
 	cd $PACKAGE_DIR/fzf
 	git pull
-	echo n | ./install --completion zsh --key-bindings zsh
+	echo n | ./install --completion --key-bindings --no-bash --no-fish
 else
 	echo "✅ fzf is already up to date. Skipping."
 fi
