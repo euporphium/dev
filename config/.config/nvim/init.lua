f8dab34ce5ca9c40cf53c460cd111cad82188e18
Daniel Newton <kdaniel.newton@gmail.com>
2025-08-16 11:25:25 -0500

    config: includes hostname in terminal prompt and sets vim tab width to 4

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -36,6 +36,11 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- (Custom) Set global indendation settings for all file types
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
